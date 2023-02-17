17dfe76e4cae04fa17f14cb272373b9608637a5a
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-18 02:34:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Dhruv Desai
+# c0883865
+
+# Assignment 4
+
+
+# 4-1 Pizza
+
+my_pizza_list = ["Margherita", "Cheese", "Pepperoni"]
+
+for name in my_pizza_list:
+    print(name)
+
+for message in my_pizza_list:
+    print("I like", message, "pizza")
+
+print("I love cheese pizza. Most of the time I order cheese pizza when I go for lunch or dinner, I really love pizza!!")
+
+# 4-2 Animal
+
+animal_list = ["Dog", "Cat", "Cow"]
+
+for name in animal_list:
+    print(name)
+
+for message in animal_list:
+    print("A", message, "would make a great pet")
+
+print("Any of these animal would make a great pet, they all have 4 legs and they all are helpful to Us!!")
+
+
+# 4-3
+
+for num in range(1,21):
+    print(num)
+
+
